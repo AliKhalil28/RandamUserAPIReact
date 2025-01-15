ba6e36f8fbc6a6cf18e69c60e0541180394b1d3e
Ali Khali <ak126pk@gmail.com>
2025-01-15 22:17:56 +0500

    fix navbar bug in Random User API

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ function App() {
     <div className="bg-gray-800 text-white min-h-[100vh]">
       <Navbar />
       <div className="container mx-auto w-[90vw] flex justify-center items-center flex-col">
-        <h1 className="text-3xl font-bold mb-6 mt-5">All Users</h1>
+        <h1 className="text-3xl font-bold mb-6 mt-[80px]">All Users</h1>
         {loading && <Loading />}
         <UserCard users={users} />
         <Pagination
